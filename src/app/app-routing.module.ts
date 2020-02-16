58b09bba56805aaa3e145e29336a640e0bb70670
Soundaryaa A <soundaryaa@Soundaryaas-MacBook-Pro.local>
2020-02-16 13:49:26 +0530

    router events

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MainPageComponent} from './main-page.component';
+import {PageNotFoundComponent} from './pagenotfound.component';
+import {HelloComponent} from './hello.component';
 
-
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+  path: 'hello',
+  component: HelloComponent
+},
+{
+  path: 'main-page',
+  component: MainPageComponent
+},
+  {
+  path: '',
+  redirectTo: '/main-page',
+  pathMatch: 'full'
+},
+{
+  path: '**',
+  component: PageNotFoundComponent
+}
+]
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    enableTracing: true
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { HelloComponent } from './hello.component';
+import {MainPageComponent} from './main-page.component';
+import {PageNotFoundComponent} from './pagenotfound.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
+  declarations: [ AppComponent, MainPageComponent, HelloComponent, PageNotFoundComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
